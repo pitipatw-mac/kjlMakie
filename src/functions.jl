5b81b8388a3960e47b8c02322a9e81c853bbba16
kjl <keithjl@mit.edu>
2022-09-12 13:08:52 -0400

    fix labelize!

--- src/functions.jl
+++ src/functions.jl
@@ -10,22 +10,22 @@ turn on/off labels for Axis3
 """
 
 function labelize!(axis::Axis3)
-    axis.titlevisible = !axis.titlevisible
-    axis.xgridvisible = !axis.xgridvisible
-    axis.xlabelvisible = !axis.xlabelvisible
-    axis.xspinesvisible = !axis.xspinesvisible
-    axis.xticklabelsvisible = !axis.xticklabelsvisible
-    axis.xticksvisible = !axis.xticksvisible
+    axis.titlevisible = !axis.titlevisible[]
+    axis.xgridvisible = !axis.xgridvisible[]
+    axis.xlabelvisible = !axis.xlabelvisible[]
+    axis.xspinesvisible = !axis.xspinesvisible[]
+    axis.xticklabelsvisible = !axis.xticklabelsvisible[]
+    axis.xticksvisible = !axis.xticksvisible[]
 
-    axis.ygridvisible = !axis.ygridvisible
-    axis.ylabelvisible = !axis.ylabelvisible
-    axis.yspinesvisible = !axis.yspinesvisible
-    axis.yticklabelsvisible = !axis.yticklabelsvisible
-    axis.yticksvisible = !axis.yticksvisible
+    axis.ygridvisible = !axis.ygridvisible[]
+    axis.ylabelvisible = !axis.ylabelvisible[]
+    axis.yspinesvisible = !axis.yspinesvisible[]
+    axis.yticklabelsvisible = !axis.yticklabelsvisible[]
+    axis.yticksvisible = !axis.yticksvisible[]
 
-    axis.zgridvisible = !axis.zgridvisible
-    axis.zlabelvisible = !axis.zlabelvisible
-    axis.zspinesvisible = !axis.zspinesvisible
-    axis.zticklabelsvisible = !axis.zticklabelsvisible
-    axis.zticksvisible = !axis.zticksvisible
+    axis.zgridvisible = !axis.zgridvisible[]
+    axis.zlabelvisible = !axis.zlabelvisible[]
+    axis.zspinesvisible = !axis.zspinesvisible[]
+    axis.zticklabelsvisible = !axis.zticklabelsvisible[]
+    axis.zticksvisible = !axis.zticksvisible[]
 end
